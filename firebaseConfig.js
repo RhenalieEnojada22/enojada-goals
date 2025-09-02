6e2069e70f7e75eb540d092e9fd8729e7e1abf64
laurence55 <laurencebarnido55@gmail.com>
2025-09-02 09:49:51 +0800

    index and create were changed

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-gGjYrmfH1vacywpmmIvHVix5lesjEDQ",
+  authDomain: "enojada-mobile-app---tracker.firebaseapp.com",
+  projectId: "enojada-mobile-app---tracker",
+  storageBucket: "enojada-mobile-app---tracker.firebasestorage.app",
+  messagingSenderId: "62152825818",
+  appId: "1:62152825818:web:0cf34f19a0ee745105bee3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
